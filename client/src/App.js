30f7ed9b9d936d4f52509bdfbd0ec4699c1a3eb2
mohamedInfath <mohamedinfath99@gmail.com>
2023-06-19 13:20:19 +0530

    page route path added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-
+import 'antd/dist/reset.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ItemPage from './pages/ItemPage';
+import CustomerPage from './pages/CustomerPage';
+import BillPage from './pages/BillPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>POS Application</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/items' element={<ItemPage />} />
+        <Route path='/customer' element={<CustomerPage />} />
+        <Route path='/bill' element={<BillPage />} />
+      </Routes>
     </div>
   );
 }
