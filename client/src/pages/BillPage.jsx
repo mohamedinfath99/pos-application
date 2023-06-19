83ffa804ab86bd04f9e12b4ffc37b67039296d54
mohamedInfath <mohamedinfath99@gmail.com>
2023-06-19 13:21:44 +0530

    inti pages created

---
+++ client/src/pages/BillPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DefaultLayout from '../components/DefaultLayout'
+
+const BillPage = () => {
+    return (
+        <DefaultLayout>
+            <h1>Bill Page</h1>
+        </DefaultLayout>
+    )
+}
+
+export default BillPage
